1f3da0cd4d91a3c21d1b2704bafac444a24a044c
kayashaolu <kay.ashaolu@gmail.com>
2018-11-09 12:46:48 -0800

    Removing api key

--- class_examples/l-storage-as-a-service/src/firebase.js
+++ class_examples/l-storage-as-a-service/src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyBhc4eNhx9Sq6vSTQgxKg-L1Dx0D7MkbCg",
+  apiKey: "[fill this in]",
   authDomain: "info-253-test-project-39206.firebaseapp.com",
   databaseURL: "https://info-253-test-project-39206.firebaseio.com",
   projectId: "info-253-test-project-39206",
